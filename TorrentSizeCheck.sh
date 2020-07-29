cdf8a0c57f38e04a7451f643a17249d8462228b2
account75783 <54882293+account75783@users.noreply.github.com>
2020-07-28 21:55:01 -0700

    Add files via upload

---
+++ TorrentSizeCheck.sh
@@ -0,0 +1,17 @@
+totalsize=0
+megabytes=0
+gigabytes=0
+size=0
+read -p "File Path or dir": dir
+for entry in "$dir"/*.torrent
+do
+count=$((count + 1))
+echo $count
+echo "$entry"
+size=$(head -1 "$entry" | grep -aoE '6:lengthi[0-9]+' | cut -di -f2 | awk '{s+=$1} END {print s}')
+megabytes=$((size / 1000000))
+gigabytes=$((size / 1000000000))
+totalsize=$((totalsize + gigabytes))
+echo "Torrent size: $megabytes MB ~ $gigabytes GB"
+done
+echo "Size of all torrents listed $totalsize GB"
