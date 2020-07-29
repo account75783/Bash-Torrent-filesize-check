a7ca43eb4b487c739de28eb7f6f4dffd903b6d78
account75783 <54882293+account75783@users.noreply.github.com>
2020-07-29 02:24:32 -0700

    Update TorrentSizeCheck.sh
    Replaced GB counter with MB counter, any files less than 1GB would not be counted, MB will count files down to 1MB.

--- TorrentSizeCheck.sh
+++ TorrentSizeCheck.sh
@@ -12,7 +12,7 @@ echo "$entry"
 size=$(head -1 "$entry" | grep -aoE '6:lengthi[0-9]+' | cut -di -f2 | awk '{s+=$1} END {print s}')
 megabytes=$((size / 1048576))
 gigabytes=$((size / 1073741824))
-totalsize=$((totalsize + gigabytes))
+totalsize=$((totalsize + megabytes))
 echo "Torrent size: $megabytes MB ~ $gigabytes GB"
 done
 fi
@@ -20,7 +20,7 @@ if [ -f "${dir}" ] ; then
 size=$(head -1 "$dir" | grep -aoE '6:lengthi[0-9]+' | cut -di -f2 | awk '{s+=$1} END {print s}')
 megabytes=$((size / 1048576))
 gigabytes=$((size / 1073741824))
-totalsize=$((totalsize + gigabytes))
+totalsize=$((totalsize + megabytes))
 echo "Torrent size: $megabytes MB ~ $gigabytes GB"
 fi
-echo "Size of all torrents listed $totalsize GB"
+echo "Size of all torrents listed $totalsize MB"
